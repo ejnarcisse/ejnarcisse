08ebaab67c444c55b412bc4a6c0d4de1778e3952
ejnarcisse <81536815+ejnarcisse@users.noreply.github.com>
2021-03-28 18:55:28 -0400

    Create BasicSettings.ps1

---
+++ BasicSettings.ps1
@@ -0,0 +1,8 @@
+$IPAddress = Read-Host "What is the IP Address? "
+$SubnetMask = Read-Hos "What is the subnet mask in CIDR formant? "
+$Gateway = read-Host "What is the gateway? "
+$Host_Name = Read-Host "What is the hostname? "
+$DNSServers = read-HOst "What is the DNS Server address seperated by commas? "
+
+New-NetIPAddress -IPAaddress $IPAddress - PrefixLength $SubnetMask -DefaultGateway $Gateway -InterfaceAlias Ethernet | Set-DnsClientServerAddress -ServerAddress $DNSServers
+Rename-Computer -NewName $Host_Name -Restart
